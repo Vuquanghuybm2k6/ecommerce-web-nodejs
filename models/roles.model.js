b96c825ab047a778e2a2dedf05a85abc511d8fe0
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-01-25 09:42:34 +0700

    Tạo model và collection role

---
+++ models/roles.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const slug = require("mongoose-slug-updater")
+mongoose.plugin(slug)
+const roleSchema = new mongoose.Schema({
+  title: String,
+  product_category_id: {
+    type: String,
+    default: ""
+  },
+  description: String,
+  permission: {
+    type: Array,
+    default: []
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: Date,
+}, {
+  timestamps: true // khi thêm trường này thì mongoose sẽ tự động thêm 2 trường createdAt và updatedAt
+});
+const Role = mongoose.model('Role', roleSchema, 'roles')
+module.exports = Role
