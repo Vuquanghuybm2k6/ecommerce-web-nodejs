63d70d98c258b91f24f790aec7fd150a57a37b0f
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-01 13:50:16 +0700

    Hiển thị danh sách sp mới nhất bên client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -2,15 +2,20 @@ const Product = require("../../models/product.model")
 const productsHelper = require("../../helper/product")
 // [GET]: /
 module.exports.index = async (req,res)=>{
-  const productFeatured = await Product.find({
+  const productsFeatured = await Product.find({
     deleted: false,
     status: "active",
     featured: "1"
-  })
-  const newproductFeatured  = productsHelper.priceNewProducts(productFeatured)
+  }).limit(6) // Lấy ra 6 sản phẩm nổi bật
+  const productsNew = await Product.find({
+    deleted: false,
+    status : "active",
+  }).limit(6).sort({position: "desc"}) // Lấy ra 6 sản phẩm mới nhất
+  const newproductsFeatured  = productsHelper.priceNewProducts(productsFeatured)
   res.render("client/pages/home/index",{
     pageTitle: "Đây là trang chủ",
-    productFeatured: newproductFeatured
+    productsFeatured: newproductsFeatured,
+    productsNew: productsNew
   }
   )
 }
