7757a4e7dbc9882d75ece427697fe01acb8d8371
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-04 10:27:52 +0700

    Tính năng đăng nhập cho client

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -21,3 +21,29 @@ module.exports.registerPost = async (req, res) => {
   }
 
 }
+// [GET]: /user/login
+module.exports.login = async (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle: "Đăng nhập tài khoản",
+  })
+}
+// [POST]: /user/register
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email
+  const password = md5(req.body.password)
+  const user = await User.findOne({email:email, deleted: false})
+  if(!user){
+    req.flash("error","Email không tồn tại")
+    return res.redirect(req.get("Referer"))
+  }
+  if(user.password != password){
+    req.flash("error","Sai mật khẩu")
+    return res.redirect(req.get("Referer"))
+  }
+  if(user.status == "inactive"){
+    req.flash("error","Tài khoản hiện đang bị khóa")
+    return res.redirect(req.get("Referer"))
+  }
+  res.cookie("tokenUser",user.tokenUser)
+  res.redirect("/")
+}
