e2721c118b1f9ff1dbb56a263a46c10a545c05b7
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-12 00:11:50 +0700

    Tính năng chat cơ bản

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,12 +1,30 @@
-
+const Chat = require("../../models/chat.model")
+const User = require("../../models/user.model")
 // [GET]: /chat
 module.exports.index = async (req,res)=>{
+  const userId = res.locals.user.id
   // SocketIO
-  _io.on('connection', (socket) => { // bắt sự kiện connect
-    console.log('a user connected', socket.id);
+  _io.once('connection', (socket) => { // bắt sự kiện connect, kết nối 1 lần duy nhất, không bị load lại, chỉ gửi 1 lần
+    socket.once("CLIENT_SEND_MESSAGE", async (content)=>{
+      // Lưu vào db
+      const chat = new Chat({
+        user_id: userId,
+        content: content
+      })
+      await chat.save()
+    })
   });  
   // End SocketIO
+  const chats = await Chat.find({deleted: false})
+  console.log(chats)
+  for(const chat of chats){
+    const infoUser = await User.findOne({
+      _id: chat.user_id
+    }).select("fullName")
+    chat.infoUser = infoUser
+  }
   res.render("client/pages/chat/index",{
-    pageTitle: "Chat",
+    pageTitle: "Chat",  
+    chats: chats
   })
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,13 @@
-var socket = io()
+// CLIENT_SEND_MESSAGE
+const formSentData = document.querySelector(".chat .inner-form")
+if(formSentData){
+  formSentData.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const content = e.target.elements.content.value
+    if(content){
+      socket.emit("CLIENT_SEND_MESSAGE", content) // bên phía controller cũng nhận được sự kiện này, sang bên đó để lấy sự kiện
+      e.target.elements.content.value = ""
+    }
+  })
+}
+// End CLIENT_SEND_MESSAGE
