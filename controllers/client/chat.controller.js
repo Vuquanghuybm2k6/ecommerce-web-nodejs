9034ae41344a0dbcc0054edf13a0f52b7250fded
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-12 08:07:22 +0700

    Chat thời gian thực

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -3,20 +3,26 @@ const User = require("../../models/user.model")
 // [GET]: /chat
 module.exports.index = async (req,res)=>{
   const userId = res.locals.user.id
+  const fullName = res.locals.user.fullName
   // SocketIO
-  _io.once('connection', (socket) => { // bắt sự kiện connect, kết nối 1 lần duy nhất, không bị load lại, chỉ gửi 1 lần
-    socket.once("CLIENT_SEND_MESSAGE", async (content)=>{
+  _io.once('connection', (socket) => { 
+    socket.on("CLIENT_SEND_MESSAGE", async (content)=>{
       // Lưu vào db
       const chat = new Chat({
         user_id: userId,
         content: content
       })
       await chat.save()
+      // Trả data về client
+      _io.emit("SERVER_RETURN_MESSAGE",{
+        userId: userId,
+        fullName: fullName,
+        content: content
+      })
     })
   });  
   // End SocketIO
   const chats = await Chat.find({deleted: false})
-  console.log(chats)
   for(const chat of chats){
     const infoUser = await User.findOne({
       _id: chat.user_id
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,34 @@
 // CLIENT_SEND_MESSAGE
-const formSentData = document.querySelector(".chat .inner-form")
-if(formSentData){
-  formSentData.addEventListener("submit", (e)=>{
+const formSendData = document.querySelector(".chat .inner-form")
+if(formSendData){
+  formSendData.addEventListener("submit", (e)=>{
     e.preventDefault()
     const content = e.target.elements.content.value
     if(content){
-      socket.emit("CLIENT_SEND_MESSAGE", content) // bên phía controller cũng nhận được sự kiện này, sang bên đó để lấy sự kiện
+      socket.emit("CLIENT_SEND_MESSAGE", content) 
       e.target.elements.content.value = ""
     }
   })
 }
 // End CLIENT_SEND_MESSAGE
+
+// SERVER_RETURN_MESSAGE
+socket.on("SERVER_RETURN_MESSAGE", (data)=>{
+  const myId = document.querySelector("[my-id]").getAttribute("my-id")
+  const body = document.querySelector(".chat .inner-body")
+  const div = document.createElement("div")
+  let htmlFullName = ""
+  if(myId == data.userId){
+    div.classList.add("inner-outgoing")
+  }
+  else{
+    div.classList.add("inner-incoming")
+    htmlFullName = `<div class = "inner-name">${data.fullName}</div>`
+  }
+  div.innerHTML = `
+  ${htmlFullName}
+  <div class = "inner-content">${data.content}</div>
+  `
+  body.appendChild(div)
+})
+// End SERVER_RETURN_MESSAGE
