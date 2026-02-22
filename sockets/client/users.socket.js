7922ea68a887cfffe279220c1e36cd63c0bc4436
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-22 09:10:25 +0700

    Tính năng xóa lời mời kết bạn

--- sockets/client/users.socket.js
+++ sockets/client/users.socket.js
@@ -4,7 +4,7 @@ module.exports = async (res) =>{
   _io.once('connection', (socket) => { 
     socket.on("CLIENT_ADD_FRIEND", async (userId)=>{
       const myUserId = res.locals.user.id 
-      // Add current user's ID into target user's acceptFriend array
+      // Thêm Id của người dùng hiện tại vào trong mảng acceptFriend của người kia
       const existUserAInB = await User.findOne({
         _id: userId,
         acceptFriends: myUserId
@@ -18,7 +18,7 @@ module.exports = async (res) =>{
           }
         })
       }
-      // Add target user's ID into current user's acceptFriends array
+      // Thêm ID của người kia vào trong acceptFriend của người dùng hiện tại
       const existUserBInA = await User.findOne({  
         _id: myUserId,
         requestFriends: userId
@@ -33,6 +33,41 @@ module.exports = async (res) =>{
         })
       }
     })
+    // Người dùng hủy gửi yêu cầu kết bạn
+    socket.on("CLIENT_CANCEL_FRIEND", async (userId)=>{
+        const myUserId = res.locals.user.id 
+        // Xóa Id của người dùng hiện tại trong mảng acceptFriend của người kia
+        const existUserAInB = await User.findOne({
+          _id: userId,
+          acceptFriends: myUserId
+        })
+        if(existUserAInB){
+          await User.updateOne({
+            _id: userId
+          }, {
+            $pull: {
+              acceptFriends: myUserId
+            }
+          })
+        }
+        // Xóa ID của người kia trong acceptFriend của người dùng hiện tại
+        const existUserBInA = await User.findOne({  
+          _id: myUserId,
+          requestFriends: userId
+        })
+        if(existUserBInA){
+          await User.updateOne({
+            _id: myUserId
+          }, {
+            $pull: {
+              requestFriends: userId
+            }
+          })
+        }
+      })
+    // End Người dùng hủy gửi yêu cầu kết bạn
   });  
+  
+
   // End SocketIO
 }
