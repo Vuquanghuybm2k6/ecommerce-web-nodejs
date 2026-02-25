a9e322edf597b20f748cec2d6a330fee6c35cc51
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-02-25 22:45:18 +0700

    Làm giao diện cho danh sách phòng chat

---
+++ controllers/client/rooms-chat.controller.js
@@ -0,0 +1,7 @@
+
+// [GET]: /rooms-chat
+module.exports.index = (req, res) => {
+  res.render("client/pages/rooms-chat/index", {
+    pageTitle: "Danh sách phòng",
+  })
+}
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -6,6 +6,7 @@ const checkoutRoutes = require("./checkout.route")
 const userRoutes = require("./user.route")
 const chatRoutes = require("./chat.route")
 const usersRoutes = require("./users.route")
+const roomsChatRoutes = require("./rooms-chat.route")
 const cartMiddleware = require("../../middlewares/client/cart.middleware")
 const categoryMiddleware = require("../../middlewares/client/category.middleware")
 const userMiddleware = require("../../middlewares/client/user.middleware")
@@ -24,4 +25,5 @@ module.exports = (app) =>{
   app.use("/user", userRoutes)
   app.use("/chat", authMiddleware.requireAuth, chatRoutes)
   app.use("/users", authMiddleware.requireAuth, usersRoutes)
+  app.use("/rooms-chat", authMiddleware.requireAuth, roomsChatRoutes)
 }
