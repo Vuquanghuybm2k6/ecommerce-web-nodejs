54ee173930b8865199a7c275a483ad8a6b49368d
Vuquanghuybm2k6 <vuquanghuybm2k6@gmail.com>
2026-07-23 09:58:29 +0700

    Cấu hình redis config

--- config/redis.js
+++ config/redis.js
@@ -2,6 +2,8 @@ const Redis = require('ioredis')
 const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || 6379,
+  username: process.env.REDIS_USER || '',
+  password: process.env.REDIS_PASSWORD || '',
   // Nếu có password: password: process.env.REDIS_PASSWORD
 })
 module.exports = redis
